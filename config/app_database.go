4132d40cac415927adf9e5a0ff0c595a8efebfb8
林 <rayln1983@hotmail.com>
2021-05-14 10:49:47 +0800

    remove database Test Table

--- config/app_database.go
+++ config/app_database.go
@@ -2,13 +2,12 @@ package config
 
 import (
 	"github.com/go-xorm/xorm"
-	"github.com/rayln/ops/entity"
 )
 
 func InitDatabase(engine *xorm.Engine) {
 	//初始化数据库表
 	err := engine.Sync(
-		new(entity.FarmTestInfo),
+	//new(entity.FarmTestInfo),
 	)
 	if err != nil {
 		panic(err.Error())
