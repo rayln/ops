15442d53b308dd02c3ac0066f3e66fd5d34c2b79
林 <rayln1983@hotmail.com>
2021-12-07 11:13:46 +0800

    增加配置文件的定义，可以设置多个数据库连接

--- util/schedule.go
+++ util/schedule.go
@@ -3,14 +3,19 @@ package util
 import "time"
 
 type Schedule struct {
+	isEnd bool
 }
 
 /**
 定时器，根据传入的时间，进行无限循环执行任务
 */
 func (that *Schedule) Run(callfunc func(), duration time.Duration) {
+	that.isEnd = false
 	go func() {
 		for {
+			if that.isEnd {
+				break
+			}
 			tiker := time.NewTicker(duration)
 			callfunc()
 			<-tiker.C
@@ -22,9 +27,13 @@ func (that *Schedule) Run(callfunc func(), duration time.Duration) {
 返回true则跳出循环
 */
 func (that *Schedule) RunToBreak(callfunc func() bool, duration time.Duration) {
+	that.isEnd = false
 	go func() {
 	Loop:
 		for {
+			if that.isEnd {
+				break
+			}
 			tiker := time.NewTicker(duration)
 			<-tiker.C
 			if callfunc() {
@@ -36,13 +45,21 @@ func (that *Schedule) RunToBreak(callfunc func() bool, duration time.Duration) {
 }
 
 func (that *Schedule) Delay(callfunc func(), duration time.Duration) {
+	that.isEnd = false
 	go func() {
 	Loop:
 		for {
+			if that.isEnd {
+				break
+			}
 			tiker := time.NewTicker(duration)
 			<-tiker.C
 			callfunc()
 			break Loop
 		}
 	}()
+}
+
+func (that *Schedule) RemoveAllSchedule() {
+	that.isEnd = true
 }
